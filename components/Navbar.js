03314ceffc97c41d44d86fb1281156eaeb5dfc9a
Anuradha Sharma <anuradhasharma71440@gmail.com>
2026-04-09 21:02:25 +0530

    style: add animated underline hover effect for navbar links

--- components/Navbar.js
+++ components/Navbar.js
@@ -11,9 +11,9 @@ export default function Navbar() {
                 <h1 className="font-semibold text-lg">DevKeep</h1>
 
                 <div className="flex gap-6 items-center text-sm">
-                    <Link href="#">Features</Link>
-                    <Link href="#">Contact</Link>
-                    <div> <ThemeToggle/></div>
+                    <Link href="/features" className="nav-link">Features</Link>
+                    <Link href="/contact" className="nav-link">Contact</Link>
+                    <div> <ThemeToggle /></div>
 
                     <button className="border border-[#8b6f5a] px-4 py-1 rounded-lg hover:bg-[#8b6f5a] hover:text-white transition">
                         Sign in
