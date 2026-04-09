9c229570fbe6701ff462e1ad066dc0885641689c
Anuradha Sharma <anuradhasharma71440@gmail.com>
2026-04-09 19:53:40 +0530

    remove default default next.js boilerplate

--- app/page.js
+++ app/page.js
@@ -1,9 +1,7 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <>
-      <div>HEllo</div>
-    </>
+    <div>Hello</div>
   );
 }
