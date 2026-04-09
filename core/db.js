2ae70d73a88a5a71a6af7be0ff22e61e88c11bbf
Anuradha Sharma <anuradhasharma71440@gmail.com>
2026-04-09 15:46:52 +0530

    fixed mongo db connection

--- core/db.js
+++ core/db.js
@@ -16,9 +16,9 @@ export async function connectDB() {
     if (cached.conn) return cached.conn;
 
     if (!cached.promise) {
+       
         cached.promise = mongoose.connect(MONGODB_URI).then((mongoose) => mongoose);
     }
-
     cached.conn = await cached.promise;
     return cached.conn;
 }
